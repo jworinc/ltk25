389fc736f2d7c2057773b5a3a631d1b3fb7b9c7b
Alexander Vantsov <alexj8784@gmail.com>
2019-05-06 16:27:17 +0300

    Stop audio on page leave

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { PlaymediaService } from './services/playmedia.service';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +11,29 @@ export class AppComponent {
   title = 'LTKLessons';
 
   constructor(
-    private el:ElementRef
+    private el:ElementRef,
+    private playmedia: PlaymediaService
   ) { }
 
+  public visible_timer: any = null;
+
   ngOnInit() {
-
-  	this.el.nativeElement.parentNode.querySelector('#app-is-loading-screen').style.display = 'none';
-
+    this.el.nativeElement.parentNode.querySelector('#app-is-loading-screen').style.display = 'none';
   }
+
+  @HostListener('document:visibilitychange', ['$event'])
+  onVisibilityChange(ev:any) {
+    if(ev.target.visibilityState === 'hidden'){
+      console.log('VisibilityChange event');
+      this.playmedia.stop();
+    }
+    
+  }
+
+  @HostListener('window:blur', ['$event'])
+  onBlurChange(ev:any) {
+    console.log('Blur Change event');
+    this.playmedia.stop();
+  }
+
 }
