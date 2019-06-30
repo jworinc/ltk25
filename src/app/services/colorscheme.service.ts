7c753426725165fc13a937d6ba04655c73b03261
Alexander Vantsov <alexj8784@gmail.com>
2019-06-30 08:40:38 +0300

    Add more colors

--- src/app/services/colorscheme.service.ts
+++ src/app/services/colorscheme.service.ts
@@ -12,7 +12,14 @@ export class ColorschemeService {
 		{card: '#FFFFFF'},
 		{card: '#2E328E'},
 		{card: '#076FB6'},
-		{card: '#39B1BC'}
+		{card: '#39B1BC'},
+		{card: '#FF91F0'},
+		{card: '#7CFFC2'},
+		{card: '#D3C57C'},
+		{card: '#FFA5A5'},
+		{card: '#DFFF91'},
+		{card: '#BABABA'},
+		{card: '#FFDE9E'}
 	];
 	public scheme_change: any;
 
