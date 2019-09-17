0c9734505f3e86d0f4b70ca1dfbe8bf51d245d2e
Alexander Vantsov <alexj8784@gmail.com>
2019-09-17 14:44:11 +0300

    Add two configurations (dev | devprod) to enable testing with dev API

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  baseApi: 'https://devapi.ltk.cards/api',
+  baseMedia: 'https://www.ltk.cards'
+};
---
+++ src/environments/environment.devprod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseApi: 'https://devapi.ltk.cards/api',
+  baseMedia: 'https://www.ltk.cards'
+};
