b880558fc2d7e3eca9325905622fa9af48781ed6
Alexander Vantsov <alexj8784@gmail.com>
2019-09-24 09:59:49 +0300

    Logout on entrance link

--- src/app/services/before-login.service.ts
+++ src/app/services/before-login.service.ts
@@ -3,19 +3,25 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angul
 import { Observable } from 'rxjs/Observable';
 import { TokenService } from './token.service';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class BeforeLoginService implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-    //return !this.Token.loggedIn();
-    if(this.Token.loggedIn()){
+    let pth = this.lcn.path().split('/');
+    let p = '';
+    if(pth.length > 1) p = pth[1];
+    if(this.Token.loggedIn() && p !== 'entrance' && p !== 'demo'){
   		this.router.navigateByUrl('/home');
   		return false;
   	} else {
+      this.Auth.changeAuthStatus(false);
+      this.Token.remove();
   		return true;
   	}
   }
-  constructor(private Token: TokenService, private router: Router) { }
+  constructor(private Token: TokenService, private router: Router, private lcn: Location, private Auth: AuthService) { }
 
 }
